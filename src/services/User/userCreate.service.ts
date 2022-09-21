feea550d0d3852755e2357ea3cf829126fe54962
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-09-21 15:48:42 -0300

    fix: error custom create

--- src/services/User/userCreate.service.ts
+++ src/services/User/userCreate.service.ts
@@ -1,8 +1,9 @@
-import { IUserCreate, IUser } from "../../interfaces/User";
+import { IUserCreate } from "../../interfaces/User";
 
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entity";
 import bcrypt from "bcrypt";
+import { AppError } from "../../errors/appError";
 
 const userCreateService = async ({
   name,
@@ -10,30 +11,24 @@ const userCreateService = async ({
   password,
   phone,
 }: IUserCreate) => {
-  // verificação de email já em uso por outro usuário
   const userRepository = AppDataSource.getRepository(User);
 
   const users = await userRepository.find();
 
   const emailAlreadyExists = users.find((user) => user.email === email);
-  // se já está em uso, invocamos um Error nativo do JS mesmo
   if (emailAlreadyExists) {
-    throw new Error("Email already exists");
+    throw new AppError(400, "Email already exists");
   }
 
-  // se não, criamos um novo usuário no modelo da interface IUser,
-  // usando os parâmetros que vamos receber lá do controller
   const user = new User();
   user.name = name;
   user.email = email;
   user.password = bcrypt.hashSync(password, 10);
   user.phone = phone;
 
-  // adicionando o novo usuário na database
   userRepository.create(user);
   await userRepository.save(user);
 
-  // retornamos o objeto do novo usuário de volta para o controller
   return user;
 };
 
