2b85149f20593a4df0d09d00b18a17af103a7dcd
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-09-23 15:54:52 -0300

    test

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import routes from "./routes";
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
