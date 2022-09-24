d03225f8bc5de1193a0e4f98dc41a3f75eda8b0c
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-09-24 09:56:46 -0300

    altered file jest.config

--- dist/src/data-source.js
+++ dist/src/data-source.js
@@ -1,30 +1,27 @@
-import { DataSource } from "typeorm";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppDataSource = void 0;
+const typeorm_1 = require("typeorm");
 require("dotenv").config();
-
-export const AppDataSource =
-  process.env.NODE_ENV === "test"
-    ? new DataSource({
+exports.AppDataSource = process.env.NODE_ENV === "test"
+    ? new typeorm_1.DataSource({
         type: "sqlite",
         database: ":memory:",
         entities: ["src/entities/*.ts"],
         synchronize: true,
-      })
-    : new DataSource({
+    })
+    : new typeorm_1.DataSource({
         type: "postgres",
         url: process.env.DATABASE_URL,
-        ssl:
-          process.env.NODE_ENV === "production"
+        ssl: process.env.NODE_ENV === "production"
             ? { rejectUnauthorized: false }
             : false,
         synchronize: false,
         logging: true,
-        entities:
-          process.env.NODE_ENV === "production"
+        entities: process.env.NODE_ENV === "production"
             ? ["dist/src/entities/*.js"]
             : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
+        migrations: process.env.NODE_ENV === "production"
             ? ["dist/src/migrations/*.js"]
             : ["src/migrations/*.ts"],
-      });
+    });
